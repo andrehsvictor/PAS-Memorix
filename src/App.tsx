5ff5108da2ac4e7978ac4ec445bfb7c55da4a885
andrehsvictor <andrehsvictor@gmail.com>
2025-03-30 06:40:19 -0300

    feat: add login page component and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/login/login-page";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
               <div className="text-3xl font-bold underline">Hello world!</div>
             }
           />
+          <Route path="login" element={<LoginPage />} />
           <Route
             path="about"
             element={<div className="text-3xl font-bold underline">About</div>}
---
+++ src/pages/login/login-page.tsx
@@ -0,0 +1,43 @@
+export default function LoginPage() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <form className="bg-white p-6 rounded shadow-md w-96">
+          <h1 className="text-4xl font-bold mb-4 text-center">Memorix</h1>
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
